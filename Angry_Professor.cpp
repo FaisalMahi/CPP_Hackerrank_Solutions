aa4d0b05098849fcce5258facc1e9a654d879fe6
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-06-15 19:41:10 +0600

    Add files via upload

---
+++ Angry_Professor.cpp
@@ -0,0 +1,34 @@
+/*https://www.hackerrank.com/challenges/angry-professor/problem*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;cin>>x;
+    while(x>0)
+    {
+            int n,k,sum=0;
+    cin>>n>>k;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]<=0)
+        {
+            sum++;
+        }
+    }
+    if(sum<k)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    x--;
+    }
+    return 0;
+}
